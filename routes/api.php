103a59c395cac9c9d6a061ce40542cea39394d83
dhtmdgkr <osh12201@gmail.com>
2020-07-29 18:11:56 +0900

    Init

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Response;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!\
+|
+*/
+
+
+Route::group(['prefix' => 'v1'], function () {
+    Route::get('auth', function(){
+        return response()->json(['ok' => true, 'message' => 'hello world!'], Response::HTTP_OK);
+    });
+});
+
+Route::fallback(function () {
+    return response()->json([
+        'ok' => false,
+        'message' => 'fallback'
+    ], Response::HTTP_NOT_FOUND);
+});
