fb7dfb3b4050376110e02e53d0a0266fd08a7e06
2020057692 <min001017@naver.com>
2021-01-20 10:04:26 -0800

    [BRACKETGG-97] member model - function (returning users) added

--- app/Models/Team/Member.php
+++ app/Models/Team/Member.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace App\Models\Team;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 /**
  * 팀의 멤버 모델 입니다.
@@ -17,9 +20,13 @@ class Member extends Model
 {
     use HasFactory;
 
+    protected $table = 'team_member';
     protected $fillable = [
-        'team_id', 'user_id',
+        'team_id', 'user_id'
     ];
 
-    protected $table = 'team_member';
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
