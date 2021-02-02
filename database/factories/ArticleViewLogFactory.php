37b44840354deb72ad5fb0cd983c430024b29f63
dhtmdgkr <osh12201@gmail.com>
2021-02-02 22:05:24 +0900

    [BRACKETGG-158] 게시글 조회 로깅 테이블 설계

---
+++ database/factories/ArticleViewLogFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\ArticleViewLog;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ArticleViewLogFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = ArticleViewLog::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
