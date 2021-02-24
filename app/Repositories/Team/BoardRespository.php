781ca5bb63289e38f0849f0c30b2cd106b513f54
dhtmdgkr123 <osh12201@gmail.com>
2021-02-24 20:47:42 +0900

    [BRACKETGG-201] 운영중인 팀 API

--- app/Repositories/Team/BoardRespository.php
+++ app/Repositories/Team/BoardRespository.php
@@ -4,8 +4,19 @@ declare(strict_types=1);
 
 namespace App\Repositories\Team;
 
+use App\Models\Team\Team;
 use App\Repositories\Common\BoardRespository as BaseBoardRepository;
+use Illuminate\Database\Eloquent\Collection;
 
 class BoardRespository extends BaseBoardRepository
 {
+    public const DEFAULT_ARTICLE_LATEST_COUNT = 10;
+
+    public function latestTenArticles(Team $team): Collection
+    {
+        return parent::latestArticles($team)->with('category')
+                                             ->orderBy((new Team())->getKeyName(), 'desc')
+                                             ->limit(self::DEFAULT_ARTICLE_LATEST_COUNT)
+                                             ->get();
+    }
 }
